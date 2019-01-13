e9b660de8f29203ea8a9f514de71fa01b12e46d5
zina-hub <yo.owca@gmail.com>
2019-01-13 18:45:29 +0100

    mostly working

--- js/equalizer.js
+++ js/equalizer.js
@@ -1,8 +1,12 @@
-const canvas = document.getElementById("canvas");
-
 window.onload = () => {
+    const canvas = document.getElementById("canvas");
+
+    const canvasContext = canvas.getContext('2d');
 
     const audioContext = new AudioContext();
+                //resume audioContext
+                if (audioContext.state == 'suspended')
+                audioContext.resume();
     const analyser = audioContext.createAnalyser();
     analyser.fftSize = 4096;
     const bufferLength = analyser.frequencyBinCount;
@@ -12,15 +16,17 @@ window.onload = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const canvasContext = canvas.getContext('2d');
     const dataArray = new Uint8Array(bufferLength);
 
     function renderFrame() {
         requestAnimationFrame(renderFrame);
         let xAxis = 0;
         analyser.getByteFrequencyData(dataArray);
-
-        canvasContext.fillStyle = "#F1FFF9";
+        
+        const gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0,"#F2B691");
+        gradient.addColorStop(1,"#FCEEB5");
+        canvasContext.fillStyle = gradient;
         canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
         const barWidth = (canvas.width / bufferLength) * 10;
@@ -29,37 +35,42 @@ window.onload = () => {
         for (let i = 0; i < bufferLength; i++) {
             let barHeight = dataArray[i] * 2.5;
 
-            let r, g, b;
+            let r, g, b, a;
 
             // Colors
             if (dataArray[i] > 200) {
                 r = 204;
                 g = 83;
                 b = 118;
+                a = 1;
             }
             else if(dataArray[i]>140){
                 r = 238;
                 g = 120;
                 b = 110;
+                a = 1;
             }
             else if(dataArray[i]>100){
                 r = 242;
                 g = 182;
                 b = 145;
+                a = 1;
             }
             else if(dataArray[i]>80){
                 r = 252;
                 g = 238;
                 b = 181;
+                a = 1;
             }
             else
             {
-                r = 162;
-                g = 204;
-                b = 182;
+                r = 241;
+                g = 255;
+                b = 249;
+                a = 1;
             }
 
-            canvasContext.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            canvasContext.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a +")";
             canvasContext.fillRect(xAxis, canvas.height - barHeight, barWidth, barHeight);
             xAxis += barWidth + barGap;
         }
@@ -79,11 +90,6 @@ window.onload = () => {
             let soundKey = soundKeys.find(soundKey => soundKey.key == keyPressed);
             soundKey.audioSrc.connect(analyser);
             analyser.connect(audioContext.destination);
-
-            //resume audioContext
-            if (audioContext.state == 'suspended')
-                audioContext.resume();
-
 
             soundKey.audio.load();
             soundKey.audio.play();
